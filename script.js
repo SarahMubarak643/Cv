d284312e5cd33b057d89ee9509cebf5e379cc719
SarahMubarak643 <sarnjssjah@gmail.com>
2025-05-06 17:04:34 +0300

    Add content to personal CV
    Added sections for languages and certificates in the HTML, with basic styles and JS functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+// Show "Back to Top" button when scrolling down
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
